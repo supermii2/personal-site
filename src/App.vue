921023bb7b3b7050032685961d10e8a4346890e9
supermii2 <alvis.ng.business@gmail.com>
2024-10-15 14:26:10 +0800

    Added Home Page and Error 404

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>
   </nav>
   <router-view/>
 </template>
 
+<script lang="ts">
+import './assets/base.css';
+
+export default {
+  name: "App",
+};
+
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 
 nav {
   padding: 30px;
+  height: 60px;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <SearchBar/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, onMounted, onBeforeUnmount } from 'vue';
+import SearchBar from '@/components/SearchBar.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+    SearchBar,
+  },
+  setup() {
+    onMounted(() => {
+      document.body.style.backgroundImage = `url(${require('@/assets/background.jpg')})`;
+      document.body.style.backgroundSize = 'cover';
+      document.body.style.backgroundPosition = 'center';
+      document.body.style.backgroundRepeat = 'no-repeat';
+    });
+
+    onBeforeUnmount(() => {
+      // Reset body styles to default or to another background
+      document.body.style.backgroundImage = '';
+      document.body.style.backgroundSize = '';
+      document.body.style.backgroundPosition = '';
+      document.body.style.backgroundRepeat = '';
+    });
   },
 });
 </script>
+
+<style scoped>
+  .home {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
